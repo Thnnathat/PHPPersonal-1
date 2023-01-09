95333ae51fec9ed8648f60e3e57115a9f377e449
Thnnathat Chaiphutha <thnnathatchaiphutha@gmail.com>
2023-01-09 18:50:17 +0700

    Last commit

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <form action="show.php"  method="post">
         Your name: <input type="text" name="name"><br>
         Your age: <input type="text" name="age"><br>
-        Your gender: <input type="radio" name="gender" id="">Male<input type="radio" name="gender" id="">Female<br>
-        Your married status: <input type="radio" name="married" id="">Married<input type="radio" name="married" id="">Unmarried
-        <br><input type="submit" value="submit">
+        Your gender: <input type="radio" name="gender" value="male">Male<input type="radio" name="gender" value="female">Female<br>
+        Your married status: <input type="radio" name="married" value="married">Married<input type="radio" name="married" value="unmarried">Unmarried<br>
+        <input type="submit" value="submit">
     </form>
 
 </body>

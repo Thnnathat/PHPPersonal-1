219d328cc5fb61b43d648bc274c7e71988d6ff82
Thnnathat <thnnathatchaiphutha@gmail.com>
2023-01-09 12:08:31 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <form action="show.php"  method="post">
+        What is your name: <input type="text" name="name"><br>
+        What is your age: <input type="text" name="age"><br>
+        What is your gender: <input type="text" name="gender"><br>
+        Your married status: <input type="text" name="married"><br>
+        <input type="submit" value="submit">
+    </form>
+
+</body>
+
+</html>
---
+++ show.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+    $name = $_POST["name"];
+    $age = $_POST["age"];
+    $gender = $_POST["gender"];
+    $marries = $_POST["marries"];
+
+    echo "Name: ".$name."<br>";
+    echo "Age: ".$age."<br>";
+    echo "Gender: ".$gender."<br>";
+
+    if ($age > 15){
+        echo "Old";
+    }else{
+        echo "";
+    }
+    ?>
+</body>
+
+</html>
